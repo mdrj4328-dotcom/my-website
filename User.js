76eccb2835edb3b8a89e44683f22b318107ecdd3
mdrj4328-dotcom <mdrj4328@gmail.com>
2026-06-11 20:41:53 +0600

    Add files via upload

--- User.js
+++ User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    facialId: { type: String }, // এটি নতুন যোগ করা হয়েছে
     files: [{ 
         filename: String, 
         category: String, 
